80ffef6e61dd45f8349e40a949060098f6e7ee2c
Diegobug <diegoifrn@gmail.com>
2021-05-07 05:05:58 -0300

    Add files via upload

--- simplex_max.py
+++ simplex_max.py
@@ -4,20 +4,17 @@ Diego Oliveira de Lemos
 """
 import numpy as np
  #gerar matriz com linhas suficientes para cada restrição e função obejtivo
-def gerar_matriz(var,rest):
-    matriz = np.zeros((rest+1, var+rest+2))
+def gerar_matriz(var,rest,artif):
+    if (artif==0):
+        matriz = np.zeros((rest+1, var+rest+2))
+    else:
+        matriz = np.zeros((rest+2, var+rest+artif+2))
     return matriz
 
-matriz = gerar_matriz(3, 2)
-matriz[0][:] = [0,3,3,2,1,0,30]
-matriz[1][:] = [0,6,3,0,0,1,48]
-matriz[2][:] = [1,-10,-8,-1,0,0,0] # f objetivo
-
-#matriz = gerar_matriz(2, 2)
-#matriz[0][:] =[0, 3, 6, 1, 0, 10]
-#matriz[1][:] =[0, 3, 3, 0, 1, 8]
-#matriz[2][:] =[1, -30,-48, 0, 0, 0] # f objetivo
-
+matriz = gerar_matriz(3, 2,0)
+#matriz[0][:] = [0,3,3,2,1,0,30]
+#matriz[1][:] = [0,6,3,0,0,1,48]
+#matriz[2][:] = [1,-10,-8,-1,0,0,0] # f objetivo
 def coluna_pivo(matriz):
     r = len(matriz[:,0])
     arrayTemp = matriz[r-1,:-1].tolist()
@@ -41,27 +38,69 @@ def linha_pivo(matriz):
         return indice
     
     
+def simplex(matriz):    
+    while coluna_pivo(matriz)!=False:   
+        linha = linha_pivo(matriz)
+        coluna = coluna_pivo(matriz)
+        print(linha)
+        print(coluna)
+
+        matriz[linha][:]= (matriz[linha][:])/matriz[linha][coluna] #atualizei a linha pivo
+        pivo = matriz[linha][coluna]
+
+        numlinhas = len(matriz) 
+        numcolunas = len(matriz[0])
+ 
+        for i in range (numlinhas):
+            n = -(matriz[i][coluna])/pivo
+            print (n)
+            for j in range (numcolunas):
+                if i!=(linha):
+                    matriz[i][j] =  matriz[linha][j]*n + matriz[i][j]
     
-while coluna_pivo(matriz)!=False:   
-    linha = linha_pivo(matriz)
-    coluna = coluna_pivo(matriz)
-    print(linha)
-    print(coluna)
+    return matriz  
+def fase2_simplex(matriz, n_artif):    
+#minimizar as variaveis artificiais (zerar)
+    return 0
 
-    matriz[linha][:]= (matriz[linha][:])/matriz[linha][coluna] #atualizei a linha pivo
-    pivo = matriz[linha][coluna]
 
+problema = "3x1+3x2+2x3<=30;6x1+3x2<=48;10x1+8x2+x3"
+_problema = problema
+for i in _problema:
+    if (i=="x"):
+        _problema = _problema.replace(i+(i+1),"")
+def build_matriz(problema):
+    temp = []
+    for x in range(len(problema)):
+        if(problema[x]=="x"):
+            temp.append(problema[x+1])
+    n_var = max(temp)
+    n_artif = problema.count(">=") + problema.count("==")
+    n_folga = problema.count("<=")
+    matriz = gerar_matriz(n_var,n_artif + n_folga, n_artif)
+    
     numlinhas = len(matriz) 
     numcolunas = len(matriz[0])
+        
+    if (n_artif>0):
+        for i in range(numlinhas):
+            for j in range(numcolunas):
+                matriz[i][j] = 0
+        fase2_simplex(matriz,n_artif)
+    else:
+        for i in range(numlinhas):
+            for j in range(numcolunas):
+                if (j==0 and i==numlinhas-1):
+                    matriz[i][j] = 1
+                elif (j==0):
+                    matriz[i][j] = 0
+        resultado_simplex(simplex(matriz))
+#matriz = gerar_matriz(2, 2)
+#matriz[0][:] =[0, 3, 6, 1, 0, 10]
+#matriz[1][:] =[0, 3, 3, 0, 1, 8]
+#matriz[2][:] =[1, -30,-48, 0, 0, 0] # f objetivo
+
  
-    for i in range (numlinhas):
-        n = -(matriz[i][coluna])/pivo
-        print (n)
-        for j in range (numcolunas):
-            if i!=(linha):
-                matriz[i][j] =  matriz[linha][j]*n + matriz[i][j]
-    
-    #print(matriz)   
 
         
         
@@ -81,5 +120,14 @@ def resultado_simplex(matriz):
     return res_simplex
 
 
+
+
+
+
+
+
+
+
+
 print(matriz)
-print(resultado_simplex(matriz))   
+print(resultado_simplex(simplex(matriz)))   
