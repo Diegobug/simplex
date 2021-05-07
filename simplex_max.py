df55f7aa52283958d64dc861528dbfe5b0438dc5
Diegobug <diegoifrn@gmail.com>
2021-05-07 10:16:16 -0300

    Add files via upload

--- simplex_max.py
+++ simplex_max.py
@@ -4,6 +4,8 @@ Diego Oliveira de Lemos
 """
 import numpy as np
  #gerar matriz com linhas suficientes para cada restrição e função obejtivo
+p = ["x1","x2","x3","x4","x5","x6","x7","x8","x9","x10","x11","x12","x13","x14","x15","x16","x17"]
+problema = "3x1+3x2+2x3<=30;6x1+3x2+0x3<=48;10x1+8x2+1x3"
 def gerar_matriz(var,rest,artif):
     if (artif==0):
         matriz = np.zeros((rest+1, var+rest+2))
@@ -11,7 +13,7 @@ def gerar_matriz(var,rest,artif):
         matriz = np.zeros((rest+2, var+rest+artif+2))
     return matriz
 
-matriz = gerar_matriz(3, 2,0)
+#matriz = gerar_matriz(3, 2,0)
 #matriz[0][:] = [0,3,3,2,1,0,30]
 #matriz[1][:] = [0,6,3,0,0,1,48]
 #matriz[2][:] = [1,-10,-8,-1,0,0,0] # f objetivo
@@ -57,56 +59,13 @@ def simplex(matriz):
             for j in range (numcolunas):
                 if i!=(linha):
                     matriz[i][j] =  matriz[linha][j]*n + matriz[i][j]
-    
+    print(matriz)
     return matriz  
 def fase2_simplex(matriz, n_artif):    
 #minimizar as variaveis artificiais (zerar)
     return 0
-
-
-problema = "3x1+3x2+2x3<=30;6x1+3x2<=48;10x1+8x2+x3"
-_problema = problema
-for i in _problema:
-    if (i=="x"):
-        _problema = _problema.replace(i+(i+1),"")
-def build_matriz(problema):
-    temp = []
-    for x in range(len(problema)):
-        if(problema[x]=="x"):
-            temp.append(problema[x+1])
-    n_var = max(temp)
-    n_artif = problema.count(">=") + problema.count("==")
-    n_folga = problema.count("<=")
-    matriz = gerar_matriz(n_var,n_artif + n_folga, n_artif)
-    
-    numlinhas = len(matriz) 
-    numcolunas = len(matriz[0])
-        
-    if (n_artif>0):
-        for i in range(numlinhas):
-            for j in range(numcolunas):
-                matriz[i][j] = 0
-        fase2_simplex(matriz,n_artif)
-    else:
-        for i in range(numlinhas):
-            for j in range(numcolunas):
-                if (j==0 and i==numlinhas-1):
-                    matriz[i][j] = 1
-                elif (j==0):
-                    matriz[i][j] = 0
-        resultado_simplex(simplex(matriz))
-#matriz = gerar_matriz(2, 2)
-#matriz[0][:] =[0, 3, 6, 1, 0, 10]
-#matriz[1][:] =[0, 3, 3, 0, 1, 8]
-#matriz[2][:] =[1, -30,-48, 0, 0, 0] # f objetivo
-
- 
-
-        
-        
-resultado_dual =[]  
-matriz = np.round(matriz,2)
 def resultado_simplex(matriz):
+    matriz = np.round(matriz,2)
     res_simplex =[]
     numlinhas = len(matriz) 
     numcolunas = len(matriz[0])
@@ -120,14 +79,77 @@ def resultado_simplex(matriz):
     return res_simplex
 
 
+        
+def build_matriz(problema):
+    temp = []
+    for x in range(len(problema)):
+        if(problema[x]=="x"):
+            temp.append(int(problema[x+1]))
+    n_var = max(temp)
+    n_artif = problema.count(">=") + problema.count("==")
+    n_folga = problema.count("<=")
+    n_folga_temp = 0
+    n_artif_temp = 0
+    linha_folgada = " "
+    coluna_folgada = " "
+    matriz = gerar_matriz(n_var,n_artif + n_folga, n_artif)
+    numlinhas = len(matriz) 
+    numcolunas = len(matriz[0])
+    
+    for x in range(len(p)):
+        if(p[x] in problema):
+            problema = problema.replace(p[x],"")
+    problema = problema.replace("<=","+")
+    problema = problema.replace(";","+")    
+    lista = problema.split("+")
+    print(lista)
+    if (n_artif>0):
+        for i in range(numlinhas):
+            for j in range(numcolunas):
+                matriz[i][j] = 0
+                
+                
+                
+        fase2_simplex(matriz,n_artif)
+        
+        
+    else:
+        for i in range(numlinhas):
+            for j in range(numcolunas):
+                if (j==0 and i==(numlinhas-1)): #coluno inicial
+                    matriz[i][j] = 1
+                if (j==0 and i!=(numlinhas-1)): #coluno inicial
+                    matriz[i][j] = 0
+                if (j>n_var and j!=(numcolunas-1) and i!=(numlinhas-1)): #preenche folga  
+                    if (n_folga_temp==i and n_folga_temp < n_folga and (str(i) not in linha_folgada)and (str(j) not in coluna_folgada)):
+                        matriz[i][j] = 1
+                        n_folga_temp+=1
+                        linha_folgada+= (str(i)+" ")
+                        coluna_folgada+= (str(j)+" ")
+                    else:
+                          matriz[i][j] = 0
+                if ((j<=n_var and j!=0) or (j==numcolunas-1)):
+                    if((j==numcolunas-1)and i==(numlinhas-1)):
+                        matriz[i][j] = 0
+                    elif (i==(numlinhas-1)):    
+                        matriz[i][j] = float(lista.pop(0))*-1
+                    else:
+                        matriz[i][j] = float(lista.pop(0))
+        print(matriz)
+        return resultado_simplex(simplex(matriz))
+   
+   
+#matriz = gerar_matriz(2, 2)
+#matriz[0][:] =[0, 3, 6, 1, 0, 10]
+#matriz[1][:] =[0, 3, 3, 0, 1, 8]
+#matriz[2][:] =[1, -30,-48, 0, 0, 0] # f objetivo
 
+ 
 
+        
+        
+resultado_dual =[]  
+print(build_matriz(problema))
 
-
-
-
-
-
-
-print(matriz)
-print(resultado_simplex(simplex(matriz)))   
+#print(matriz)
+#print(resultado_simplex(simplex(matriz)))   
